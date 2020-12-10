7810be36c5762a6710a5249aa14b84ed8b4eb443
Darya Gurina <malevich187@gmail.com>
2020-12-11 01:47:17 +0300

    vladimirvictorovichzdraste

---
+++ Practice/27/C++/TOP-5minimum/TOP-5minimum.cpp
@@ -0,0 +1,57 @@
+﻿#include<iostream>
+#include<vector>
+#include<locale>
+#include <algorithm> // for sort
+
+std::vector <int> HUBABUBA(std::vector <int> b, int n)
+{
+	int u = 0;
+	sort(b.begin(), b.end(), std::greater<int>());
+	if ((b.size() < 5))
+	{
+		for (int i = 0; i < n; i++)
+		{
+			if (i < 5)
+			{
+				std::cout << b[i] << ' ';
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+	else
+	{
+		for (int i = b.size() - 5; i < b.size(); i++)
+		{
+			std::cout << b[i] << ' ';
+		}
+	}
+	std::cout << "\n";
+	return b;
+}
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int n, c;
+	c = 0;
+	std::cout << "Введите количество сигналов\n";
+	std::cin >> n;
+	std::cout << "Введите сигналы\n";
+	std::vector <int> a(n);
+	std::vector <int> b(0);
+	std::vector <int> s(0);
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> a[i];
+	}
+	for (int i = 0; i < a.size(); i++)
+	{
+		b.push_back(a[i]);
+		int n = b.size();
+		HUBABUBA(b, n);
+
+	}
+	return 0;
+}
---
+++ Practice/28/C++/FactorizationofaNaturalNumber/FactorizationofaNaturalNumber.cpp
@@ -0,0 +1,40 @@
+﻿#include<vector>
+#include <iostream>
+#include <algorithm>
+#include<map>
+void print_factorization(unsigned int n)
+{
+	std::map <unsigned int, unsigned int> dict;
+	int div = 2;
+	while (n > 1)
+	{
+		for (unsigned int i = 2; i <= n; i++)
+		{
+			if (!(n % i))
+			{
+				n /= i;
+
+				dict[i] = dict.count(i) ? dict[i] + 1 : 1;
+				break;
+			}
+		}
+	}
+	std::cout << "1";
+	for (auto now : dict)
+	{
+		if (now.second > 1)
+		{
+			std::cout << "*" << now.first << "^" << now.second;
+		}
+		else
+		{
+			std::cout << "*" << now.first;
+		}
+	}
+}
+int main()
+{
+	unsigned int n;
+	std::cin >> n;
+	print_factorization(n);
+}
